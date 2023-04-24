7831798de11ed25e52efff28085033f99ee661c4
hugo ratts <vhugoratts@gmail.com>
2023-04-24 20:46:09 -0300

    1.0.0

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ const height = parseInt(args[3]);
 percorrer_pastas(diretorio, marca_dagua, width, height)
     .then(() => console.log('Marca d\'água adicionada com sucesso!'))
     .catch((e) => console.log(e));
+
+if (require.main === module) {
+    const { percorrer_pastas } = require("./controllers/RecursiveController.js");
+
+    const args = process.argv.slice(2);
+    const diretorio = args[0];
+    const marca_dagua = args[1];
+    const width = parseInt(args[2]);
+    const height = parseInt(args[3]);
+
+    percorrer_pastas(diretorio, marca_dagua, width, height)
+        .then(() => console.log('Marca d\'água adicionada com sucesso!'))
+        .catch((e) => console.log(e));
+}
